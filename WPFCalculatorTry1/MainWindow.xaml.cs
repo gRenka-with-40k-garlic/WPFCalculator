4414f76dc877d38e436850cb68eb06b698b19fc6
gRenka <128580487+gRenka-with-40k-garlic@users.noreply.github.com>
2024-01-13 12:22:30 +0700

    ...φ(・∀・*)

---
+++ WPFCalculatorTry1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCalculatorTry1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement element in Buttons.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button)element).Click += ButtonClick;
+                }
+            }
+        }
+
+        private void ButtonClick(Object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string TextButton = ((Button)e.OriginalSource).Content.ToString();
+
+                //string TextButton = ((Button)e.OriginalSource).Content.ToString();
+
+                if (TextButton == "Clear all")
+                {
+                    text.Clear();
+                }
+                else if (TextButton == "Save last")
+                {
+                    text.Text = text.Text.Substring(text.Text.Length - 1);
+                }
+                else if (TextButton == "=")
+                {
+                    text.Text = new DataTable().Compute(text.Text, null).ToString();
+                }
+                else text.Text += TextButton;
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
